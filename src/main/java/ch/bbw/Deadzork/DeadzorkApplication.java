d51b638f9d2dbb1fa987ca0b59a44a6ade54f3bd
Giuliano Gianola <giuliano.gianola@swisscom.com>
2023-01-19 07:52:57 +0100

    Initial commit

---
+++ src/main/java/ch/bbw/Deadzork/DeadzorkApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.Deadzork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadzorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadzorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/Deadzork/DeadzorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.Deadzork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadzorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
